fb0d1c3338e81f658b39667ce4a76d10f16b8046
Marko Gargenta <marko@marakana.com>
2012-02-14 16:59:28 -0800

    Added about

---
+++ Yamba/src/com/marakana/android/yamba/StatusActivity.java
@@ -0,0 +1,93 @@
+package com.marakana.android.yamba;
+
+import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class StatusActivity extends Activity implements OnClickListener {
+    private static final String TAG = "StatusActivity";
+    
+    private Twitter twitter;
+    private EditText statusMsg;
+    private Toast toast;
+
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+		Log.v(TAG, "onCreate() invoked");
+        
+        statusMsg = (EditText) findViewById(R.id.edit_msg);
+        Button updateButton = (Button) findViewById(R.id.button_update);
+        updateButton.setOnClickListener(this);
+        
+        toast = Toast.makeText(this, null, Toast.LENGTH_LONG);
+        
+//        System.setProperty("http.proxyHost", "host");
+//        System.setProperty("http.proxyPort", "port_number");
+//
+//        If proxy requires authentication,
+//
+//        System.setProperty("http.proxyUser", "user");
+//        System.setProperty("http.proxyPassword", "password");
+        
+        twitter = new Twitter("student", "password");
+        twitter.setAPIRootUrl("http://yamba.marakana.com/api");
+    }
+
+	public void onClick(View v) {
+		int id = v.getId();
+		switch (id) {
+		case R.id.button_update:
+			// User clicked the Update Status button
+			Log.v(TAG, "Button clicked");
+			String msg = statusMsg.getText().toString();
+			Log.v(TAG, "User entered: " + msg);
+			statusMsg.setText("");
+			
+			if (!TextUtils.isEmpty(msg)) {
+				// Post the message to the server
+				new PostToTwitter().execute(msg);
+			}
+			
+			break;
+		default:
+			// Unknown button! We should never get here!
+		}
+	}
+	
+	private class PostToTwitter extends AsyncTask<String, Void, Integer> {
+
+		@Override
+		protected Integer doInBackground(String... params) {
+			int result = R.string.post_status_success;
+			try {
+				twitter.setStatus(params[0]);
+			} catch (TwitterException e) {
+				Log.w(TAG, "Failed to post message", e);
+				result = R.string.post_status_fail;
+			}
+			return result;
+		}
+
+		@Override
+		protected void onPostExecute(Integer result) {
+			toast.setText(result);
+			toast.show();
+		}
+		
+	}
+
+}
+
+
