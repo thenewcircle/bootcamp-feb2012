ccd4c26b2c19168e6bf4c974e7c55a78c11ccca4
Marko Gargenta <marko@marakana.com>
2012-02-16 19:31:59 -0800

    Implemented AlarmManager to run refreshes

--- Yamba/src/com/marakana/android/yamba/BootReceiver.java
+++ Yamba/src/com/marakana/android/yamba/BootReceiver.java
@@ -1,17 +1,29 @@
 package com.marakana.android.yamba;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
 public class BootReceiver extends BroadcastReceiver {
-
+	static final long INTERVAL = 10000; // 10 seconds
+	
 	/** Called when the intent we subscribed for gets broadcasted. */
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		context.startService( new Intent(context, RefreshService.class) );
+		// Create pending intent, i.e. our operation to submit to the alarm
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0,
+				new Intent(context, RefreshService.class),
+				PendingIntent.FLAG_UPDATE_CURRENT);
+
+		// Setup alarm
+		AlarmManager alarmManager = (AlarmManager) context
+				.getSystemService(Context.ALARM_SERVICE);
+		alarmManager.setInexactRepeating(AlarmManager.RTC,
+				System.currentTimeMillis(), INTERVAL, pendingIntent);
+
 		Log.d("BootReceiver", "onReceive");
 	}
-
 }
