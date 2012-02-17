7b14cea215f492448bb420513ebdd632ff00721d
Marko Gargenta <marko@marakana.com>
2012-02-16 18:09:35 -0800

    Added exception handling

--- Yamba/src/com/marakana/android/yamba/RefreshService.java
+++ Yamba/src/com/marakana/android/yamba/RefreshService.java
@@ -3,6 +3,7 @@ package com.marakana.android.yamba;
 import java.util.List;
 
 import winterwell.jtwitter.Twitter.Status;
+import winterwell.jtwitter.TwitterException;
 import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
@@ -23,15 +24,19 @@ public class RefreshService extends IntentService {
 	protected void onHandleIntent(Intent intent) {
 		// Gets 20 most recent statuses from us and our friends in the last 24
 		// hours
-		List<Status> timeline = ((YambaApp) getApplication()).getTwitter()
-				.getFriendsTimeline();
+		try {
+			List<Status> timeline = ((YambaApp) getApplication()).getTwitter()
+					.getFriendsTimeline();
 
-		// Iterate of timeline
-		for (Status status : timeline) {
-			Log.d(TAG, String.format("%s: %s", status.user.name, status.text));
-			if (getContentResolver().insert(StatusProvider.CONTENT_URI,
-					StatusProvider.getValues(status)) != null)
-				count++;
+			// Iterate of timeline
+			for (Status status : timeline) {
+				Log.d(TAG, String.format("%s: %s", status.user.name, status.text));
+				if (getContentResolver().insert(StatusProvider.CONTENT_URI,
+						StatusProvider.getValues(status)) != null)
+					count++;
+			}
+		} catch (TwitterException e) {
+			Log.e(TAG, "Failed to get timeline", e);
 		}
 
 		Log.d(TAG, "onHandleIntent");
